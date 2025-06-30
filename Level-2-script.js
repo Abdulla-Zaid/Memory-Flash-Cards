84db083cf338f552b396b81dabd34768fd6f55a1
Abdulla-Zaid <abdulla4433@gmail.com>
2025-06-30 23:49:01 +0300

    copy code from Level 1 to Level 2 then increase row and colum

---
+++ Level-2-script.js
@@ -0,0 +1,72 @@
+////////////////////////////////
+// Global Variables Here
+const Modes = document.querySelectorAll('li')
+const BoxCards = document.querySelectorAll('th')
+Cards = []
+let Counter = -1
+let WonFlag = true
+let TimmerFlag = false
+////////////////////////////////
+// Functions For Game Logic Here
+
+const FlipCards = () => {
+  for (let i = 0; i < BoxCards.length; i++) {
+    document.querySelectorAll('img')[i].style.opacity = '1'
+  }
+}
+
+const FlipCardsBack = () => {
+  for (let i = 0; i < BoxCards.length; i++) {
+    document.querySelectorAll('img')[i].style.opacity = '0'
+  }
+  TimmerFlag = true
+}
+
+function CheckWinner() {
+  if (Cards[0] && Cards[3]) {
+    document.querySelectorAll('h1')[0].style.backgroundColor = '#2ead2e'
+    document.querySelectorAll('h1')[0].innerText = 'You Won!'
+    document.querySelectorAll('a')[0].href = './Level-2.html'
+    document.querySelector('.Next-Level').style.backgroundColor = '#2ead2e'
+    WonFlag = false
+  } else {
+    document.querySelectorAll('h1')[0].style.backgroundColor = '#ff0000'
+    document.querySelectorAll('h1')[0].innerText = 'You lost Game Over!'
+    document.querySelectorAll('a')[0].href = './Level-2.html'
+    WonFlag = false
+  }
+}
+
+const FlipTheCards = (Number) => {
+  if (WonFlag && TimmerFlag) {
+    document.querySelectorAll('img')[Number].style.opacity = '1'
+    document.querySelectorAll('th')[Number].style.backgroundColor = 'green'
+    Cards[Number] = true
+    Counter++
+    if (Counter === 1) {
+      CheckWinner()
+    }
+  }
+}
+
+////////////////////////////////
+// Event Listeners Here
+
+for (let i = 0; i < BoxCards.length; i++) {
+  BoxCards[i].addEventListener('click', () => FlipTheCards(i))
+}
+
+Modes[0].addEventListener('click', () => {
+  Modes[0].style.backgroundColor = '#03e203'
+  Modes[1].style.backgroundColor = '#efff0d'
+  setTimeout(FlipCards, 1000)
+  setTimeout(FlipCardsBack, 4000)
+})
+Modes[1].addEventListener('click', () => {
+  Modes[0].style.backgroundColor = '#efff0d'
+  Modes[1].style.backgroundColor = '#03e203'
+  setTimeout(FlipCards, 1000)
+  setTimeout(FlipCardsBack, 2000)
+})
+
+////////////////////////////////
