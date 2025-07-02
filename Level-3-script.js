5b5a0bdb5159c9ce0dd1ddf44cfb799d112be624
Abdulla-Zaid <abdulla4433@gmail.com>
2025-07-02 18:04:39 +0300

    link all pages to home page

--- Level-3-script.js
+++ Level-3-script.js
@@ -69,13 +69,13 @@ for (let i = 0; i < BoxCards.length; i++) {
   BoxCards[i].addEventListener('click', () => FlipTheCards(i))
 }
 
-Modes[0].addEventListener('click', () => {
+Modes[1].addEventListener('click', () => {
   Modes[0].style.backgroundColor = '#03e203'
   Modes[1].style.backgroundColor = '#efff0d'
   setTimeout(FlipCards, 1000)
   setTimeout(FlipCardsBack, 4000)
 })
-Modes[1].addEventListener('click', () => {
+Modes[2].addEventListener('click', () => {
   Modes[0].style.backgroundColor = '#efff0d'
   Modes[1].style.backgroundColor = '#03e203'
   setTimeout(FlipCards, 1000)
